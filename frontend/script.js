b804cb77e796c8f87dd53d45797262f3ed9827ec
ricsimarta <ricsimarta@gmail.com>
2022-01-25 15:26:55 +0100

    trying to make things work

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,47 @@
-function prezi() {
-    const szoveg = document.querySelector("#main")
-    szoveg.removeChild(szoveg.childNodes[0])
-    szoveg.insertAdjacentHTML("beforeend",`<p><button id="visszagomb"><-</button>prezipreziprezi </p>`)
+const kiirando = `<p><button id="visszagomb" onclick="vissza()">←</button><button id="eloregomb">→</button> proba szoveg </p>`
 
+const kmnev = 'Keresztes Mirella'
+const kmszoveg = 'km szovegek'
+
+const mrnev = 'Márta Richárd'
+const mrszoveg ='ide fogom irni a dolgokat'
+
+const bbnev = 'Buczkó Balázs'
+const bbszoveg = 'bb szoveg'
+
+const kbnev = 'Kovács Bence'
+const kbszoveg = 'kb szoveg'
+
+function prezi() {
+    const fo = document.getElementById("main")
+    fo.innerHTML = ""
+    fo.insertAdjacentHTML("beforeend", kiirando)
+}
+
+function bemutatkozo(nev, szoveg) {
+    const fo = document.getElementById("main")
+    fo.innerHTML = ""
+    fo.insertAdjacentHTML("beforeend", `<p><button id="visszagomb" onclick="vissza()">←</button><button id="eloregomb">→</button><span id="nev">${nev}</span><span>${szoveg}</span></p>`)
+}
+
+function vissza() {
+    const fo = document.getElementById("main")
+    fo.innerHTML = ""
+    fo.insertAdjacentHTML("beforeend", kiirando)
+}
+
+function km() {
+    bemutatkozo(kmnev, kmszoveg)
+}
+
+function mr() {
+    bemutatkozo(mrnev, mrszoveg)
+}
+
+function bb() {
+    bemutatkozo(bbnev, bbszoveg)
+}
+
+function kb() {
+    bemutatkozo(kbnev, kbszoveg)
 }
